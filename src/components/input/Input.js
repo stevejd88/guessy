f0c9a24ccf1312f471857f80ad6fd996ff56d9f8
Steven De Avila <stevejd88@gmail.com>
2020-11-04 14:03:13 -0600

    Set up Input component and storeFactory util

---
+++ src/components/input/Input.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const Input = () => {
+  return <div></div>;
+};
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+export default connect(mapStateToProps)(Input);
